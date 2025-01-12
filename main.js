a6822dc1fde85731157f6a2f1ce8fe71718787ca
Jebaraj <jeba.j@live.com>
2025-01-12 21:04:21 +0530

    check in with auth added

--- main.js
+++ main.js
@@ -2,19 +2,29 @@ const API_URL = 'https://script.google.com/macros/s/AKfycbwUMhXBImWs4m9tgn4WR_ma
 
 function fetchStudentDetails() {
     let input = document.getElementById('searchInput').value;
-    if (input.length < 5 && !input.startsWith("TC")) {
-        input = "TC" + input;
+    if (input.length < 5 && !input.startsWith("KC")) {
+        input = "KC" + input;
     }
 
     document.getElementById('backdrop').style.display = 'flex';
-
-    fetch(`${API_URL}?action=getStudentDetails&input=${input}`)
+    const accessTokenField = document.querySelector("#accessToken");
+    fetch(`${API_URL}?action=getStudentDetails&input=${input}&authToken=${accessTokenField.value}`)
         .then(response => response.json())
-        .then(data => {
+        .then(result => {
             document.getElementById('backdrop').style.display = 'none';
-            displayStudentDetails(data.data);
+            if (result.status.toLowerCase() === 'success') {
+                displayStudentDetails(result.data);
+            } else {
+                const messageElement = document.getElementById('checkInModalMessage');
+                messageElement.innerText = result.message;
+                messageElement.className = 'text-danger';
+                const modal = new bootstrap.Modal(document.getElementById('checkInModal'));
+                modal.show();
+            }
+
         })
-        .catch(() => {
+        .catch((error) => {
+            console.log(error)
             document.getElementById('backdrop').style.display = 'none';
             const messageElement = document.getElementById('checkInModalMessage');
             messageElement.innerText = 'Error retrieving data';
@@ -51,7 +61,8 @@ function displayStudentDetails(details) {
 function checkIn(regRefNo) {
     document.getElementById('backdrop').style.display = 'flex';
 
-    fetch(`${API_URL}?action=checkInStudent&regRefNo=${regRefNo}`)
+    const accessTokenField = document.querySelector("#accessToken");
+    fetch(`${API_URL}?action=checkInStudent&regRefNo=${regRefNo}&authToken=${accessTokenField.value}`)
         .then(response => response.json())
         .then(result => {
             document.getElementById('backdrop').style.display = 'none';
@@ -79,3 +90,66 @@ function checkIn(regRefNo) {
             modal.show();
         });
 }
+
+
+auth0.createAuth0Client({
+    domain: "dev-sazlz3uf0genwd7a.us.auth0.com",
+    clientId: "APU6p14IiXfP3Q83B1c8VvPy4cZ2YAH9",
+    authorizationParams: {
+        redirect_uri: window.location.origin + "/ss-events/index.html"
+    }
+}).then(async (auth0Client) => {
+
+// Assumes a button with id "login" in the DOM
+// const loginButton = document.querySelector("#loginBtn");
+
+// loginButton.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     auth0Client.loginWithRedirect();
+// });
+
+if (location.search.includes("state=") &&
+    (location.search.includes("code=") ||
+    location.search.includes("error="))) {
+    await auth0Client.handleRedirectCallback();
+    window.history.replaceState({}, document.title, "/ss-events");
+}
+
+// Assumes a button with id "logout" in the DOM
+const logoutButton = document.getElementById("logout");
+
+logoutButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    //auth0Client.logout();
+     auth0Client.logout({
+        async onRedirect(url) {
+          window.location.replace(url);
+        }
+      });
+    //auth0Client.loginWithRedirect();
+});
+
+const isAuthenticated = await auth0Client.isAuthenticated();
+//const userProfile = await auth0Client.getUser();
+
+// Assumes an element with id "profile" in the DOM
+//const profileElement = document.getElementById("profile");
+
+if (isAuthenticated) {
+    logoutButton.style.display = "block";
+    //profileElement.style.display = "block";
+    // profileElement.innerHTML = `
+    //         <p>${userProfile.name}</p>
+    //         <img src="${userProfile.picture}" />
+    //     `;
+} else {
+    //profileElement.style.display = "none";
+    logoutButton.style.display = "none";
+    auth0Client.loginWithRedirect();
+}
+const accessTokenField = document.querySelector("#accessToken");
+const accessToken = await auth0Client.getTokenSilently();
+accessTokenField.value = accessToken;
+
+console.log(accessToken)
+});
